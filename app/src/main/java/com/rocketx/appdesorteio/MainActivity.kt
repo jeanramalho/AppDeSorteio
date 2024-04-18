7ba03275d188d4b33b9b35d10f254dd09a6c8241
Jean Ramalho <jeanramalho.dev@gmail.com>
2024-04-18 13:19:07 -0300

    MainActivity.kt[add] - Cria funções do aplicativo

--- app/src/main/java/com/rocketx/appdesorteio/MainActivity.kt
+++ app/src/main/java/com/rocketx/appdesorteio/MainActivity.kt
@@ -2,10 +2,21 @@ package com.rocketx.appdesorteio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun sortear(view: View) {
+        val textoResultado = findViewById<TextView>(R.id.text_resultado)
+        val numero = Random().nextInt(101)
+
+        textoResultado.setText("Número Gerado $numero")
+    }
+
 }
